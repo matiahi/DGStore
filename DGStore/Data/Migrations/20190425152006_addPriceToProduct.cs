8ff8b4eac0bda4e8a761af5ebb0bfbbb0080088e
nona.matiahi@gmail.com <nona.matiahi@gmail.com>
2019-04-25 20:02:07 +0430

    addPriceToProductTable

---
+++ DGStore/Data/Migrations/20190425152006_addPriceToProduct.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DGStore.Data.Migrations
+{
+    public partial class addPriceToProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+        }
+    }
+}
