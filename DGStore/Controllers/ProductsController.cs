0dba6c45c0d0d0619a7040d9ea2f17918ae24c44
nona.matiahi@gmail.com <nona.matiahi@gmail.com>
2019-04-25 20:00:26 +0430

    createProductTable

---
+++ DGStore/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DGStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DGStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        private readonly ApplicationDbContext _context;
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+
+            var list = _context.Products.ToList();
+            return View(list);
+        }
+
+    }
+}
---
+++ DGStore/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DGStore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        public short NumberInStock { get; set; }
+        public bool IsExists { get; set; }
+        [Required]
+        public string Specification { get; set; }
+        public int Price { get; set; }
+
+    }
+}
